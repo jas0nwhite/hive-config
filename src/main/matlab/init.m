ea00712c3b434f171aa3dc5a075e6936197d80db
Jason White <jas0nw@vtc.vt.edu>
2016-03-15 15:57:04 -0400

    add init.m script

---
+++ src/main/matlab/init.m
@@ -0,0 +1,14 @@
+% restore default matlab path
+restoredefaultpath;
+
+% clear out auto-generated class definitions (just in case)
+clear Config Chem hive.cfg.TargetCatalog hive.cfg.TestingCatalog hive.cfg.TrainingCatalog; 
+
+% clear variables
+clear;
+
+% clear screen
+clc;
+
+% config
+cfg = Config.init;
