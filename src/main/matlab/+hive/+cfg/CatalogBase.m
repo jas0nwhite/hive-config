f3f17ccd016da2fefbcbfff32d90b1add05cb1f3
Jason White <jas0nw@vtc.vt.edu>
2016-03-15 15:57:12 -0400

    cleanup

--- src/main/matlab/+hive/+cfg/CatalogBase.m
+++ src/main/matlab/+hive/+cfg/CatalogBase.m
@@ -1,12 +1,12 @@
 classdef (Abstract) CatalogBase
     % base class for catalogs
-
+    
     properties (Abstract, Constant)
-    	sourceSpecList
-	resultPathList
-	voltammetryWindow
-	sourceCatalog
-	datasetCatalog
-   end
-
+        sourceSpecList
+        resultPathList
+        voltammetryWindow
+        sourceCatalog
+        datasetCatalog
+    end
+    
 end
