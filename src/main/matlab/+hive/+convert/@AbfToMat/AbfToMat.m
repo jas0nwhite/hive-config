796eb776a37f3bb463e7d2e3e7069006f3df363c
Jason White <jas0nw@vtc.vt.edu>
2016-03-16 11:35:43 -0400

    added ABF file converter

---
+++ src/main/matlab/+hive/+convert/@AbfToMat/AbfToMat.m
@@ -0,0 +1,120 @@
+classdef AbfToMat < hive.util.Logging
+    %ABFTOMAT Converter for ABF files to MATLAB files
+    
+    properties (Constant)
+        SUCCESS = 0
+        FAILURE = 1
+        SKIPPED = 2
+    end
+    
+    properties (Access = protected)
+        inputFiles
+        outputFile
+        metadataFile
+        sampleWindow
+        timeWindow
+    end
+    
+    %
+    % constructors
+    %
+    methods
+        function this = AbfToMat(inputFiles, outputFile, metadataFile, sampleWindow, timeWindow)
+            this.inputFiles = inputFiles;
+            this.outputFile = outputFile;
+            this.metadataFile = metadataFile;
+            this.sampleWindow = sampleWindow;
+            this.timeWindow = timeWindow;
+        end
+    end
+    
+    %
+    % API
+    %
+    methods
+        function status = convert(this)
+            nFiles = length(this.inputFiles);
+            
+            % check files
+            for ix = 1:nFiles
+                this.checkFile(this.inputFiles{ix});
+            end
+            
+            % allocate cells for outputs
+            data = cell(nFiles, 1);
+            header = cell(nFiles, 1);
+            fSamp = NaN(nFiles, 1);
+            fSweep = NaN(nFiles, 1);
+            nSamp = NaN(nFiles, 1);
+            nSweep = NaN(nFiles, 1);
+                        
+            parfor ix = 1:nFiles
+                [data{ix}, fSamp(ix), fSweep(ix), header{ix}] = this.readAbf(ix); %#ok<PFBNS,PFOUS>
+                [nSamp(ix), nSweep(ix)] = size(data{ix}); %#ok<PFOUS>
+            end
+            
+            save(this.outputFile, 'data');
+            
+            % copy vars for save
+            files = this.inputFiles; %#ok<NASGU>
+            samples = this.sampleWindow; %#ok<NASGU>
+            times = this.timeWindow; %#ok<NASGU>
+            
+            save(this.metadataFile, ...
+                'fSamp', 'fSweep', 'header', 'nSamp', 'nSweep', 'files', 'samples', 'times');
+            
+            status = this.SUCCESS;
+        end
+    end
+    
+    %
+    % external functions
+    %
+    methods (Static)
+        [d, si, h] = abfload(fn, varargin)
+    end
+    
+    %
+    % internal API
+    %
+    methods (Access = protected)
+        
+        function [data, fSamp, fSweep, header] = readAbf(this, ix)
+            abfFile = this.inputFiles{ix};
+            
+            % load the data from the ABF file
+            [data, sampInterval, header] = hive.convert.AbfToMat.abfload(abfFile);
+            
+            % convert data to convenient format
+            sampInterval = sampInterval * 1e-6; % seconds
+            data = squeeze(data); % remove extra dimensions
+            
+            % calculate the time window
+            if isnan(this.timeWindow)
+                sweepWindow = 1:size(data, 2);
+            else
+                sweepStart = min(this.timeWindow);
+                sweepEnd = max(this.timeWindow);
+                
+                sweepInterval = sampInterval * size(data, 1);
+                sweepStartIx = max(1, round(sweepStart / sweepInterval) + 1);
+                sweepEndIx = min(size(data, 2), round(sweepEnd / sweepInterval));
+                
+                sweepWindow = sweepStartIx:sweepEndIx;
+            end
+            
+            % extract the data
+            if isnan(this.sampleWindow)
+                data = data(:, sweepWindow);
+            else
+                data = data(this.sampleWindow, sweepWindow);
+            end
+            
+            fSamp = 1/sampInterval;
+            fSweep = 1/sweepInterval;
+        end
+        
+    end
+    
+end
+
---
+++ src/main/matlab/+hive/+util/Logging.m
@@ -0,0 +1,72 @@
+classdef (Abstract) Logging
+    %LOGGING Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+        function warn(this, id, message, varargin)
+            this.doWarn(3, id, message, varargin{:})
+        end
+        
+        function error(this, id, message, varargin)
+            this.doError(3, id, message, varargin{:})
+        end
+        
+        function checkFile(this, file, shouldContinue)
+            if nargin < 3
+                shouldContinue = false;
+            end
+            
+            if ~ exist(file, 'file')
+                if (shouldContinue)
+                    this.doWarn(3, 'FileNotFound', 'file "%s" does not exist', file);
+                else
+                    this.doError(3, 'FileNotFound', 'file "%s" does not exist', file);
+                end
+            end
+        end
+    end
+    
+    methods (Access = protected)
+        function doWarn(~, ix, id, message, varargin)
+            [cls, fn] = hive.util.Logging.currentFrame(ix);
+            msgId = sprintf('HIVE:%s', id);
+            warning(msgId, [message ' (in %s:%s)'], varargin{:}, cls, fn);
+        end
+        
+        function doError(~, ix, id, message, varargin)
+            [cls, fn] = hive.util.Logging.currentFrame(ix);
+            msgId = sprintf('HIVE:%s', id);
+            error(msgId, [message ' (in %s:%s)'], varargin{:}, cls, fn);
+        end
+    end
+    
+    methods (Static, Access = protected)
+        function [cls, fn] = currentFrame(ix)
+            if nargin == 0
+                ix = 1;
+            end
+            
+            dbk = dbstack( 1, '-completenames' );
+            if isempty(dbk)
+                str = 'base';
+            else
+                ix = min(ix, length(dbk));
+                str = dbk(ix).name;
+            end
+            cac = regexp( str, '\.', 'split' );
+            switch  numel( cac )
+                case 1
+                case 2
+                    %cls = meta.class.fromName( cac{1} );
+                    cls = cac{1};
+                    fn = cac{2};
+                case 3
+                otherwise
+            end
+        end
+    end
+end
+
