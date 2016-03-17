f64da63ad85ec192626f15baa61aa597a6d802c2
Jason White <jas0nw@vtc.vt.edu>
2016-03-16 21:01:51 -0400

    first work on training data preprocessor

--- src/main/matlab/+hive/+proc/+train/Preprocessor.m
+++ src/main/matlab/+hive/+proc/+train/Preprocessor.m
@@ -2,10 +2,47 @@ classdef Preprocessor
     %PREPROCESSOR Summary of this class goes here
     %   Detailed explanation goes here
     
-    properties
+    properties (Access = protected)
+        cfg
+        labels
     end
     
+    %
+    % API
+    %
     methods
+        
+        function this = Preprocessor(cfg)
+            this.cfg = cfg;
+            this.labels = readtable(this.cfg.training.labelCatalogFile);
+        end
+        
+        function this = process(this)
+            nSets = size(this.cfg.training.sourceCatalog);
+            
+            for setIx = 1:nSets
+                nSources = size(this.cfg.training.sourceCatalog{setIx}, 1);
+                
+                for sourceIx = 1:nSources
+                    this.processSource(setIx, sourceIx)
+                end
+            end
+        end
+        
+        
+        
+    end
+    
+    %
+    % internal API
+    %
+    methods (Access = protected)
+        
+        function processSource(this, setIx, sourceIx)
+            
+            
+        end
+        
     end
     
 end
