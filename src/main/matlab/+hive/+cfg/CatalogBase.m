385fc13ab7f3e3b641ef74babc6dda73d344ba44
Jason White <jas0nw@vtc.vt.edu>
2016-03-17 11:22:02 -0400

    added catalog methods for accessing data using absolute or relative indexing extracted status variables to a separate class (enumeration) added overwrite option to converter and preprocessor started work at adding label collation to ABF converter

--- src/main/matlab/+hive/+cfg/CatalogBase.m
+++ src/main/matlab/+hive/+cfg/CatalogBase.m
@@ -4,9 +4,51 @@ classdef (Abstract) CatalogBase
     properties (Abstract, Constant)
         sourceSpecList
         resultPathList
-        voltammetryWindow
+        vgramWindowList
         sourceCatalog
         datasetCatalog
+    end
+    
+    %
+    % API
+    %
+    methods
+        
+        function [id, name, directory] = getSourceInfo(this, setIx, sourceIx)
+            if nargin == 2
+                [setIx, sourceIx] = this.getSourceIxByDatasetId(setIx);
+            end
+            
+            id = this.datasetCatalog{setIx}{sourceIx, 1};
+            name = this.datasetCatalog{setIx}{sourceIx, 2};
+            directory = this.sourceCatalog{setIx}{sourceIx, 2};
+        end
+        
+        function [setIx, sourceIx] = getSourceIxByDatasetId(this, datasetId)
+            locations = cellfun(@(c) find(vertcat(c{:, 1}) == datasetId), this.datasetCatalog,...
+                'UniformOutput', false);
+            
+            setIx = find(cellfun(@(v) ~isempty(v), locations));
+            sourceIx = locations{setIx};
+        end
+        
+        function val = getSetValue(~, setList, setIx)
+            nVals = size(setList, 1);
+            
+            if setIx > nVals
+                val = setList{1};
+            else
+                val = setList{setIx};
+            end
+        end
+        
+    end
+    
+    %
+    % internal API
+    %
+    methods (Access = protected)        
+        
     end
     
 end
