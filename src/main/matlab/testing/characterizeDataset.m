3b23cdd46927f8559968a5ce786e08b1b53ae071
Jason White <jas0nw@vtc.vt.edu>
2016-04-04 16:29:37 -0400

    best effort so far at clustering & characterization

--- src/main/matlab/testing/characterizeDataset.m
+++ src/main/matlab/testing/characterizeDataset.m
@@ -9,11 +9,21 @@ function [coreIx, noiseIx, borderIx, clustIx] = characterizeDataset(catalog, dat
     summary = load(summaryFile);
     metadata = load(metadataFile);
 
-    scanList = summary.steps.median;
-    reference = summary.grand.median;
+    scanList = summary.steps.mean;
+    reference = summary.grand.mean;
+    stdDev = summary.grand.std;
+    cv = stdDev ./ abs(reference);
+
+    if (epsPct == 0)
+        epsPct = median(cv(1:60)) * 150;
+    end
 
     if (nargin == 3)
-        minPoints = ceil(length(scanList) * 0.75);
+        minPoints = 3;
+    end
+
+    if (minPoints < 1)
+        minPoints = ceil(minPoints * length(scanList));
     end
 
     c = characterize(scanList, reference);
@@ -26,69 +36,133 @@ function [coreIx, noiseIx, borderIx, clustIx] = characterizeDataset(catalog, dat
     normY = y / refY;
 
     normXY = horzcat(normX, normY);
-    [clustIx, lc] = dbscan(normXY, epsPct, minPoints);
+    eps = epsPct/100;
+
+    [clustIx, lc] = dbscan(normXY, eps, minPoints);
 
     coreIx = find(lc > 0);
     noiseIx = find(lc == -1);
     borderIx = find(lc == -2);
+    clusteredIx = sort(union(coreIx, borderIx));
+    nClust = length(unique(clustIx(coreIx)));
 
     scanMat = cell2mat(scanList');
     time = metadata.sampleIx{1} ./ metadata.sampleFreq(1) * 1e3; % milliseconds
 
-    figure;
 
-    subplot(2, 1, 1);
+        function resizeAxis(~, ~, ax)
+            axes(ax)
+            axis equal
+
+            % Uncomment next two lines for same axes sizes while printing
+            % set(ax, 'DataAspectRatioMode', 'auto')
+            % set(ax, 'Plotboxaspectratiomode', 'auto')
+        end
+
+
+    f = figure;
+
+    colors = colormap(lines(nClust));
+
+    %
+    % cluster plot
+    %
+    eq = subplot(2, 2, [2 4]);
     hold on;
-    
-    if ~isempty(coreIx)
-        plot(normX(coreIx), normY(coreIx), 'ob', 'MarkerFaceColor', 'b');
-    end
 
     if ~isempty(borderIx)
-        plot(normX(borderIx), normY(borderIx), 'oc', 'MarkerFaceColor', 'c');
+        plot(normX(borderIx)*100, normY(borderIx)*100, 'oc', 'MarkerFaceColor', 'c', 'MarkerSize', 15);
+    end
+
+    if ~isempty(coreIx)
+        for i = 1:nClust
+            plotIx = clustIx == i;
+            plot(normX(plotIx)*100, normY(plotIx)*100, 'o', ...
+                'MarkerEdgeColor', colors(i, :), 'MarkerFaceColor', colors(i, :));
+        end
     end
 
     if ~isempty(noiseIx)
-        plot(normX(noiseIx), normY(noiseIx), 'or');
+        plot(normX(noiseIx)*100, normY(noiseIx)*100, 'or');
     end
-    
+
     theta = 0:pi/50:2*pi;
-    circX = epsPct/2 * cos(theta) + 1;
-    circY = epsPct/2 * sin(theta) + 1;
+    circX = epsPct * cos(theta) + 100;
+    circY = epsPct * sin(theta) + 100;
     plot(circX, circY, ':k');
-    
-    title('characterization');
+
+    title(sprintf('clusters: %d, retained: %d, rejected: %d', nClust, length(clusteredIx), length(noiseIx)));
     xlabel('knot location (% ref)');
     ylabel('knot coefficient (% ref)');
+    axis equal;
+
+    hold off;
+
+    set(f, 'ResizeFcn',{@resizeAxis, eq})
+    % set(f, 'PaperPositionMode', 'auto') %Avoid resizing the figure when printing
+
+
+
+    %
+    % CV plot
+    %
+    subplot(2, 2, 1);
+    hold on;
+
+    plotCV = cv;
     
+    plot(time, plotCV);
+    title('dataset coefficients of variation');
+    xlabel('time (ms)');
+    ylabel('CV (%)');
+    axis tight;
+    ylim([0, .25]);
+
+    plot([time(1) time(1)], get(gca, 'YLim'), 'b:');
+    plot([time(60) time(60)], get(gca, 'YLim'), 'b:');
+    plot([time(1) time(60)], [epsPct epsPct] ./ 100, 'k:');
+
+    xlim([1, 12]);
+
     hold off;
 
 
-
-    subplot(2, 1, 2);
+    %
+    % voltammogram plot
+    %
+    subplot(2, 2, 3);
     hold on;
-
-    plot([time(1) time(1)], [-500 1500], 'g:');
-    plot([time(60) time(60)], [-500 1500], 'g:');
-
+    
     if ~isempty(coreIx)
-        plot(time, scanMat(:, coreIx), 'b');
+        for i = 1:nClust
+            plotIx = clustIx == i;
+            plot(time, scanMat(:, plotIx), 'Color', colors(i, :));
+        end
     end
-
-    if ~isempty(borderIx)
-        plot(time, scanMat(:, borderIx), 'c');
-    end
-
+    
+    % if ~isempty(borderIx)
+    %     plot(time, scanMat(:, borderIx), 'c');
+    % end
+    
     if ~isempty(noiseIx)
         plot(time, scanMat(:, noiseIx), 'r');
     end
-
-    plot(time, reference, 'k', 'LineWidth', 2);
-    title('voltammograms');
+    
+    % plot(time, reference, 'k', 'LineWidth', 2);
+    
+    title('dataset voltammograms');
     xlabel('time (ms)');
     ylabel('current (nA)');
+    axis tight;
+    
+    plot([time(1) time(1)], get(gca, 'YLim'), 'b:');
+    plot([time(60) time(60)], get(gca, 'YLim'), 'b:');
+    
     xlim([1, 12]);
-    ylim([-2100, 2100]);
 
-    suptitle(sprintf('dataset #%d: %s', datasetIx, strrep(name, '_', '-')));
+
+    suptitle(sprintf(...
+        'dataset #%d: %s\nepsilon = %0.1f%%, min cluster size = %0d', ...
+        datasetIx, strrep(name, '_', '-'), epsPct, minPoints));
+    
 end
