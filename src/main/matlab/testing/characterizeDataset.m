84fa93e6e6ecc1adc6271ddd52f2544b5378b64a
Jason White <jas0nw@vtc.vt.edu>
2016-04-05 12:52:21 -0400

    switched to zscore normalization added cluster plotting added SLM fit plotting

--- src/main/matlab/testing/characterizeDataset.m
+++ src/main/matlab/testing/characterizeDataset.m
@@ -1,4 +1,4 @@
-function [coreIx, noiseIx, borderIx, clustIx] = characterizeDataset(catalog, datasetIx, epsPct, minPoints)
+function [coreIx, noiseIx, borderIx, clustIx] = characterizeDataset(catalog, datasetIx, eps, minPoints)
 
     [setIx, sourceIx] = catalog.getSourceIxByDatasetId(datasetIx);
     [~, name, ~] = catalog.getSourceInfo(setIx, sourceIx);
@@ -12,10 +12,9 @@ function [coreIx, noiseIx, borderIx, clustIx] = characterizeDataset(catalog, dat
     scanList = summary.steps.mean;
     reference = summary.grand.mean;
     stdDev = summary.grand.std;
-    cv = stdDev ./ abs(reference);
-
-    if (epsPct == 0)
-        epsPct = median(cv(1:60)) * 150;
+    
+    if (eps == 0)
+        eps= 1;
     end
 
     if (nargin == 3)
@@ -29,14 +28,13 @@ function [coreIx, noiseIx, borderIx, clustIx] = characterizeDataset(catalog, dat
     c = characterize(scanList, reference);
     x = cellfun(@(s) s.knots(2), c.slm);
     y = cellfun(@(s) s.coef(2), c.slm);
-    refX = c.refSlm.knots(2);
-    refY = c.refSlm.coef(2);
+    % refX = c.refSlm.knots(2);
+    % refY = c.refSlm.coef(2);
 
-    normX = x / refX;
-    normY = y / refY;
+    normX = zscore(x);
+    normY = zscore(y);
 
     normXY = horzcat(normX, normY);
-    eps = epsPct/100;
 
     [clustIx, lc] = dbscan(normXY, eps, minPoints);
 
@@ -65,35 +63,97 @@ function [coreIx, noiseIx, borderIx, clustIx] = characterizeDataset(catalog, dat
     colors = colormap(lines(nClust));
 
     %
+    % SLM plot
+    %
+    subplot(2, 2, 4);
+    hold on;
+    
+    for ix = 1:length(c.slm)
+        model = c.slm{ix};
+        
+        xrange = model.knots([1, end]);
+        xev = linspace(xrange(1), xrange(2), 1001);
+        ypred = slmeval(xev, model);
+        
+        h = plot(model.x, model.y);        
+        set(h, ...
+            'LineStyle', '-', ...
+            'Marker', 'none', ...
+            'Color', [0.8 0.8 0.8]);
+        
+        h = plot(xev, ypred);
+        set(h, ...
+            'Marker', 'none', ...
+            'Color', [1.0 0.8 0.8], ...
+            'LineStyle', '-', ...
+            'LineWidth', 0.5);
+        
+        axlim = axis;
+        yrange = axlim(3:4);
+        knots = model.knots(:);
+        
+        h = plot(repmat(knots', 2, 1), yrange(:));
+        set(h, ...
+            'Marker', 'none', ...
+            'Color', [0.8 1.0 0.8], ...
+            'LineStyle', '--');
+    end
+    
+    plot(x, y, 'LineStyle', 'none', 'Marker', 'o', 'MarkerEdgeColor', 'none', 'MarkerFaceColor', colors(1, :));
+    
+    xlim([min(x) * 0.9, max(x) * 1.1]);
+    ylim([min(y) * 0.9, max(y) * 1.1]);
+    
+    title('characterization (SLM)');
+    xlabel('sample #');
+    ylabel('current (nA)');
+    hold off;
+    
+    
+    %
     % cluster plot
     %
-    eq = subplot(2, 2, [2 4]);
+    eq = subplot(2, 2, 2);
     hold on;
 
     if ~isempty(borderIx)
-        plot(normX(borderIx)*100, normY(borderIx)*100, 'oc', 'MarkerFaceColor', 'c', 'MarkerSize', 15);
+        plot(normX(borderIx), normY(borderIx), 'oc', 'MarkerFaceColor', 'c', 'MarkerSize', 15);
     end
 
+    theta = linspace(0, 2*pi, 100);
+    cx = eps * cos(theta);
+    cy = eps * -sin(theta); % -sin(theta) to make a clockwise contour
+    
     if ~isempty(coreIx)
+    
+        for i = 1:nClust
+            patchIx = find(lc == i);
+            px = [];
+            py = [];
+           
+            for j = 1:length(patchIx)
+                ix = patchIx(j);
+                [px, py] = polybool('union', px, py, cx + normX(ix), cy + normY(ix));
+            end
+            
+            patch(px, py, colors(i, :), 'FaceAlpha', .1, 'EdgeColor', colors(i, :));
+        end
+        
         for i = 1:nClust
             plotIx = clustIx == i;
-            plot(normX(plotIx)*100, normY(plotIx)*100, 'o', ...
+            plot(normX(plotIx), normY(plotIx), 'o', ...
                 'MarkerEdgeColor', colors(i, :), 'MarkerFaceColor', colors(i, :));
         end
+        
     end
 
     if ~isempty(noiseIx)
-        plot(normX(noiseIx)*100, normY(noiseIx)*100, 'or');
+        plot(normX(noiseIx), normY(noiseIx), 'or');
     end
 
-    theta = 0:pi/50:2*pi;
-    circX = epsPct * cos(theta) + 100;
-    circY = epsPct * sin(theta) + 100;
-    plot(circX, circY, ':k');
-
     title(sprintf('clusters: %d, retained: %d, rejected: %d', nClust, length(clusteredIx), length(noiseIx)));
-    xlabel('knot location (% ref)');
-    ylabel('knot coefficient (% ref)');
+    xlabel('knot location (\sigma)');
+    ylabel('knot coefficient (\sigma)');
     axis equal;
 
     hold off;
@@ -104,23 +164,21 @@ function [coreIx, noiseIx, borderIx, clustIx] = characterizeDataset(catalog, dat
 
 
     %
-    % CV plot
+    % SDT plot
     %
     subplot(2, 2, 1);
     hold on;
 
-    plotCV = cv;
-    
-    plot(time, plotCV);
-    title('dataset coefficients of variation');
+    plot(time, stdDev);
+    title('dataset standard deviation');
     xlabel('time (ms)');
-    ylabel('CV (%)');
+    ylabel('\sigma (nA)');
+    
     axis tight;
-    ylim([0, .25]);
-
+    
     plot([time(1) time(1)], get(gca, 'YLim'), 'b:');
     plot([time(60) time(60)], get(gca, 'YLim'), 'b:');
-    plot([time(1) time(60)], [epsPct epsPct] ./ 100, 'k:');
+    %plot([time(1) time(60)], [eps eps], 'k:');
 
     xlim([1, 12]);
 
@@ -162,7 +220,7 @@ function [coreIx, noiseIx, borderIx, clustIx] = characterizeDataset(catalog, dat
 
 
     suptitle(sprintf(...
-        'dataset #%d: %s\nepsilon = %0.1f%%, min cluster size = %0d', ...
-        datasetIx, strrep(name, '_', '-'), epsPct, minPoints));
+        'dataset #%d: %s\nepsilon = %0.2f, min cluster size = %0d', ...
+        datasetIx, strrep(name, '_', '-'), eps, minPoints));
     
 end
