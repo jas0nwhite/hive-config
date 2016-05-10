36e890719f500467679e63a56a0f1e20868ba455
Jason White <jas0nw@vtc.vt.edu>
2016-05-10 15:05:02 -0400

    first pass at clustring voltammograms

--- src/main/matlab/testing/characterize.m
+++ src/main/matlab/testing/characterize.m
@@ -4,14 +4,23 @@ function dat = characterize(scanList)
     fit = cell(nScans, 1);
 
     for ix = 1:length(scanList)
-        scan = scanList{ix}(1:60);
+        % extract window of interest (WoI)
+        if iscell(scanList)
+            scan = scanList{ix}(1:60);
+        else
+            scan = scanList(1:60, ix);
+        end
+        
         nx = length(scan);
         
+        % "resample" with spline to create smoother output
         xp = linspace(1, nx, 3000);
         yp = interp1(1:nx, scan, xp, 'spline');
 
+        % subtract linear trend defined by endpoints of WoI
         yy = yp - linspace(yp(1), yp(end), length(yp));
 
+        % find location and value of maximum
         maxIx = ceil(median(find(yy == max(yy))));
         
         x0 = xp(maxIx);
