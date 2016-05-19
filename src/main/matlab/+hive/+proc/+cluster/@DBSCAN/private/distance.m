0765cf0446aef88dc65793513517ddbe281eff3e
Jason White <jas0nw@vtc.vt.edu>
2016-05-19 10:32:22 -0400

    cleanup / formatting

--- src/main/matlab/+hive/+proc/+cluster/@DBSCAN/private/distance.m
+++ src/main/matlab/+hive/+proc/+cluster/@DBSCAN/private/distance.m
@@ -1,13 +1,18 @@
-function d = distance(X,Y, dosqrt)
-% function d = distance(X,Y, dosqrt);
+function d = distance(X, Y, dosqrt)
+% function d = distance(X, Y, dosqrt);
 % dosqrt: default 0: No square root to reduce computation time (only for comparation perpose)
 % Euclidean distance matrix between row vectors in X and Y
+%
+% NOTE: can't replace this with pdist2() due to the possibility of NaN
+% values
 
-if nargin < 3, dosqrt = 0; end
+    if nargin < 3, dosqrt = 0; end
 
-U=~isnan(Y); Y(~U)=0;
-V=~isnan(X); X(~V)=0;
-if ~dosqrt d=abs(X.^2*U'+V*Y'.^2-2*X*Y');
-else
-    d=sqrt(X.^2*U'+V*Y'.^2-2*X*Y');
-end
+    U = ~isnan(Y); Y(~U) = 0;
+    V = ~isnan(X); X(~V) = 0;
+    
+    if ~dosqrt
+        d = abs(X.^2*U' + V*Y'.^2 - 2*X*Y');
+    else
+        d = sqrt(X.^2*U' + V*Y'.^2 - 2*X*Y');
+    end
