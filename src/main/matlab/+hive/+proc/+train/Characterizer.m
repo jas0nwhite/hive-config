4c7171137a2fd5b352bad2ab59a04d46129259e4
Jason White <jas0nw@vtc.vt.edu>
2016-05-26 11:47:24 -0400

    refactored processors to be reused for testing datasets

--- src/main/matlab/+hive/+proc/+train/Characterizer.m
+++ src/main/matlab/+hive/+proc/+train/Characterizer.m
@@ -1,4 +1,4 @@
-classdef Characterizer < hive.proc.ProcessorBase
+classdef Characterizer < hive.proc.CharacterizerBase
     %CHARACTERIZER Summary of this class goes here
     %   Detailed explanation goes here
     
@@ -13,7 +13,7 @@ classdef Characterizer < hive.proc.ProcessorBase
         function this = Characterizer(cfg)
             this.treatment = cfg;
             this.cfg = cfg.training;
-            this.actionLabel = 'Characterizing';
+            this.actionLabel = 'Characterizing training';
         end
         
     end
@@ -22,53 +22,6 @@ classdef Characterizer < hive.proc.ProcessorBase
     % IMPLEMENTATION
     %
     methods (Access = protected)
-        
-        function argv = getArgsForProcessSource(this, setIx)
-            argv = {length(this.cfg.datasetCatalog{setIx})};
-        end
-        
-        function processSource(this, setIx, sourceIx, nSources)
-            [id, name, ~] = this.cfg.getSourceInfo(setIx, sourceIx);
-            
-            fprintf('    dataset %03d (%d files): %s... ', id, nSources, name);
-            t = tic;
-            
-            directory = fullfile(this.cfg.resultPathList{setIx}, this.cfg.datasetCatalog{setIx}{sourceIx, 2});
-            
-            infile = fullfile(directory, this.cfg.vgramFile);
-            outfile = fullfile(directory, this.cfg.characterizationFile);
-            
-            if this.overwrite || ~exist(outfile, 'file')
-                load(infile);
-                
-                nSteps = length(voltammograms); %#ok<USENS>
-                
-                vgramChar = cell(nSteps, 1);
-                
-                fprintf('%s %d steps...', this.actionLabel, nSteps);
-                
-                for stepIx = 1:nSteps
-                    vgramChar{stepIx} = this.processStep(voltammograms{stepIx});
-                end
-                
-                save(outfile, 'vgramChar');
-                
-                fprintf(' %0.3fs\n', toc(t));
-            else
-                fprintf('SKIP.\n');
-            end
-        end
-        
-        function c = processStep(~, vgrams)
-            nVgrams = size(vgrams, 2);
-            
-            c = nan(nVgrams, 2);
-            
-            for vgramIx = 1:nVgrams
-                [x, y] = hive.proc.analyze.characterizeVoltammogram(vgrams(:, vgramIx));
-                c(vgramIx, :) = [x, y];
-            end
-        end
         
     end
     
