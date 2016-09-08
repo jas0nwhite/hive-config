2f0586461a973fcf90f40574356a900dbc5f46c7
Jason White <jas0nw@vtc.vt.edu>
2016-09-08 10:10:39 -0400

    code to gather CVerr objects & data from each crossvalidation (for Rosalyn)

---
+++ src/main/matlab/testing/gatherCVerr.m
@@ -0,0 +1,40 @@
+function [ CVerr, labels, chemicals ] = gatherCVerr( cfg )
+%GATHERCVERR Summary of this function goes here
+%   Detailed explanation goes here
+    %%
+    nSets = size(cfg.training.sourceCatalog, 1);
+    
+    CVerr = cell(nSets, 1);
+    labels = cell(nSets, 1);
+    chemicals = cell(nSets, 1);
+    
+    %%
+    for setIx = 1:nSets
+        %%
+        nDatasets = size(cfg.training.sourceCatalog{setIx}, 1);
+        modelPath = cfg.testing.resultPathList{setIx};
+        
+        CVerr{setIx} = cell(nDatasets, 1);
+        labels{setIx} = cell(nDatasets, 1);
+        chemicals{setIx} = cell(nDatasets, 1);        
+        %%
+        for sourceIx = 1:nDatasets
+            %%
+            [~, name, ~] = cfg.training.getSourceInfo(setIx, sourceIx);
+            
+            modelFile = fullfile(modelPath, name, 'cv-model.mat');
+            trainingFile = fullfile(modelPath, name, 'cv-training.mat');
+            
+            model = load(modelFile);
+            CVerr{setIx, sourceIx} = model.CVerr;
+            
+            info = load(trainingFile, 'labels', 'chemical');
+            labels{setIx, sourceIx} = unique(info.labels, 'rows');
+            chemicals{setIx, sourceIx} = info.chemical;
+            
+        end
+        
+    end
+
+end
+
