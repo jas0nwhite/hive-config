af3985a0d81af35331db4334b6e8cb277117f619
Jason White <jas0nw@vtc.vt.edu>
2017-04-13 14:39:39 -0400

    first light: use scodec-stream to parse ABF file

---
+++ src/main/scala/org/hnl/abf2/structs/FileGUID.scala
@@ -0,0 +1,29 @@
+package org.hnl.abf2.structs
+
+import scodec._
+import scodec.bits._
+import scodec.codecs._
+import scodec.codecs.implicits._
+import scala.language.implicitConversions
+
+/**
+ * FileGUID
+ * <p>
+ * Created on Apr 14, 2017.
+ * <p>
+ *
+ * @author Jason White
+ */
+case class FileGUID(Data1: Long, Data2: Int, Data3: Int, Data4: Vector[Int]) {
+  require(Data4.length == 8)
+}
+object FileGUID {
+  implicit val codec: Codec[FileGUID] = {
+    (
+      ("Data1" | uint32L) ::
+      ("Data2" | uint16L) ::
+      ("Data3" | uint16L) ::
+      ("Data4" | vectorOfN(provide(8), uint8))
+    ).as[FileGUID]
+  }
+}
