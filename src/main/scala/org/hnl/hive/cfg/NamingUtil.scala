11f4bc2e6ae4ca4e8354fd245f410407e8eb0798
Jason White <jas0nw@vtc.vt.edu>
2019-01-08 11:44:50 -0500

    added support for enumerating datasets where the same protocol is executed multiple times in the same day

--- src/main/scala/org/hnl/hive/cfg/NamingUtil.scala
+++ src/main/scala/org/hnl/hive/cfg/NamingUtil.scala
@@ -36,11 +36,12 @@ object NamingUtil extends Logging {
     }
   }
 
-  val altq      = """(?:_alt)?"""
-  val fastq     = """(?:_[0-9]+Vs_[0-9]+Hz(?:_corrected)?)?"""
+  val altq = """(?:_alt)?"""
+  val fastq = """(?:_[0-9]+Vs_[0-9]+Hz(?:_corrected)?)?"""
   val octaflowq = """(?:_octaflow)?"""
-  val bypassq   = """(?:_bypass)?"""
-  val uncorrq   = s"""(?:_(?:uncorrelated|RBV[^_]*)${octaflowq}${bypassq}_[0-9]+k_[0-9]+Hz)?"""
+  val bypassq = """(?:_bypass)?"""
+  val uncorrq = s"""(?:_(?:uncorrelated|RBV[^_]*)${octaflowq}${bypassq}_[0-9]+k_[0-9]+Hz)?"""
+  val ordinalq = """(?:_\d{1})?"""
 
   val dateP = """\d{4}_\d{2}_\d{2}"""
   val nameP = """(?:[ap]m\d?_)?(?:[A-Za-z]+|[A-Za-z]+_[A-Za-z]+|[A-Za-z0-9]+)"""
@@ -52,18 +53,14 @@ object NamingUtil extends Logging {
   val phP = s"""pH_[LH]{3}${fastq}${altq}${uncorrq}"""
   val randomP = """(?:increased_|decreased_)?random_high_(?:DA|5HT)(?:_[0-9])?"""
   val mixtureP = s"""(?:(?:DA|5HT|NE|5HIAA|pH)_?){2,5}${octaflowq}${fastq}${uncorrq}"""
-  // val threeXP = s"""DA_5HT_NE${octaflowq}${fastq}${uncorrq}"""
-  // val fourXP = s"""DA_5HT_NE_pH${octaflowq}${fastq}${uncorrq}"""
 
-  val pH = new Regex(s"""(${dateP})_(${phP})_(${nameP})_?(${dateP})?""")
-  val dopamine = new Regex(s"""(${dateP})_(${dopamineP})_(${nameP})_?(${dateP})?""")
-  val serotonin = new Regex(s"""(${dateP})_(${serotoninP})_(${nameP})_?(${dateP})?""")
-  val norepi = new Regex(s"""(${dateP})_(${norepiP})_(${nameP})_?(${dateP})?""")
-  val hiaa = new Regex(s"""(${dateP})_(${hiaaP})_(${nameP})_?(${dateP})?""")
-  val random = new Regex(s"""(${dateP})_(${randomP})_(${nameP})_?(${dateP})?""")
-  val mixture = new Regex(s"""(${dateP})_(${mixtureP})_?(${nameP})?_?(${dateP})?""")
-  // val threeX = new Regex(s"""(${dateP})_(${threeXP})_?(${nameP})?_?(${dateP})?""")
-  // val fourX = new Regex(s"""(${dateP})_(${fourXP})_?(${nameP})?_?(${dateP})?""")
+  val pH = new Regex(s"""(${dateP})_(${phP})_(${nameP})_?(${dateP})?${ordinalq}?""")
+  val dopamine = new Regex(s"""(${dateP})_(${dopamineP})_(${nameP})_?(${dateP})?${ordinalq}?""")
+  val serotonin = new Regex(s"""(${dateP})_(${serotoninP})_(${nameP})_?(${dateP})?${ordinalq}?""")
+  val norepi = new Regex(s"""(${dateP})_(${norepiP})_(${nameP})_?(${dateP})?${ordinalq}?""")
+  val hiaa = new Regex(s"""(${dateP})_(${hiaaP})_(${nameP})_?(${dateP})?${ordinalq}?""")
+  val random = new Regex(s"""(${dateP})_(${randomP})_(${nameP})_?(${dateP})?${ordinalq}?""")
+  val mixture = new Regex(s"""(${dateP})_(${mixtureP})_?(${nameP})?_?(${dateP})?${ordinalq}?""")
 
   def datasetInfoFromName(dataset: String): Option[InvitroDataset] = {
     dataset match {
@@ -81,8 +78,8 @@ object NamingUtil extends Logging {
       case pH(dsDate, dsProtocol, probeName, probeDate)        => Some(InvitroDataset(dsDate, "pH", dsProtocol, probeName, probeDate))
       case dopamine(dsDate, dsProtocol, probeName, probeDate)  => Some(InvitroDataset(dsDate, "dopamine", dsProtocol, probeName, probeDate))
       case serotonin(dsDate, dsProtocol, probeName, probeDate) => Some(InvitroDataset(dsDate, "serotonin", dsProtocol, probeName, probeDate))
-      case norepi(dsDate, dsProtocol, probeName, probeDate)         => Some(InvitroDataset(dsDate, "norepinephrine", dsProtocol, probeName, probeDate))
-      case hiaa(dsDate, dsProtocol, probeName, probeDate)           => Some(InvitroDataset(dsDate, "5-hydroxyindoleacetic acid", dsProtocol, probeName, probeDate))
+      case norepi(dsDate, dsProtocol, probeName, probeDate)    => Some(InvitroDataset(dsDate, "norepinephrine", dsProtocol, probeName, probeDate))
+      case hiaa(dsDate, dsProtocol, probeName, probeDate)      => Some(InvitroDataset(dsDate, "5-hydroxyindoleacetic acid", dsProtocol, probeName, probeDate))
       case random(dsDate, dsProtocol, probeName, probeDate)    => Some(InvitroDataset(dsDate, "mixture", dsProtocol, probeName, probeDate))
       case mixture(dsDate, dsProtocol, probeName, probeDate)   => Some(InvitroDataset(dsDate, "mixture", dsProtocol, probeName, probeDate))
 
--- src/test/scala/org/hnl/hive/cfg/NamingUtilSpec.scala
+++ src/test/scala/org/hnl/hive/cfg/NamingUtilSpec.scala
@@ -22,45 +22,45 @@ class NamingUtilSpec extends WordSpec with Matchers with Inspectors with OptionV
 
       option1 should not be (null)
       option1 shouldBe defined
-      option1.value should have(
-        dsDate("2015_07_14"),
-        dsClass("dopamine"),
-        dsProtocol("dopamine"),
-        probeName("A_EL"),
-        probeDate("2015_07_07"))
+      option1.value should have(dsDate("2015_07_14"),
+                                dsClass("dopamine"),
+                                dsProtocol("dopamine"),
+                                probeName("A_EL"),
+                                probeDate("2015_07_07")
+      )
 
       val option2 = NamingUtil.datasetInfoFromName("2015_10_27_dopamine_am_KK_2013_07_29")
 
       option2 should not be (null)
       option2 shouldBe defined
-      option2.value should have(
-        dsDate("2015_10_27"),
-        dsClass("dopamine"),
-        dsProtocol("dopamine"),
-        probeName("am_KK"),
-        probeDate("2013_07_29"))
+      option2.value should have(dsDate("2015_10_27"),
+                                dsClass("dopamine"),
+                                dsProtocol("dopamine"),
+                                probeName("am_KK"),
+                                probeDate("2013_07_29")
+      )
 
       val option3 = NamingUtil.datasetInfoFromName("2015_11_12_pH_HLH_pm3_KK_2013_07_29")
 
       option3 should not be (null)
       option3 shouldBe defined
-      option3.value should have(
-        dsDate("2015_11_12"),
-        dsClass("pH"),
-        dsProtocol("pH_HLH"),
-        probeName("pm3_KK"),
-        probeDate("2013_07_29"))
+      option3.value should have(dsDate("2015_11_12"),
+                                dsClass("pH"),
+                                dsProtocol("pH_HLH"),
+                                probeName("pm3_KK"),
+                                probeDate("2013_07_29")
+      )
 
       val option4 = NamingUtil.datasetInfoFromName("2017_03_09_5HT_uncorrelated_octaflow_bypass_100k_97Hz_V8")
 
       option4 should not be (null)
       option4 shouldBe defined
-      option4.value should have(
-        dsDate("2017_03_09"),
-        dsClass("serotonin"),
-        dsProtocol("5HT_uncorrelated_octaflow_bypass_100k_97Hz"),
-        probeName("V8"),
-        probeDate(""))
+      option4.value should have(dsDate("2017_03_09"),
+                                dsClass("serotonin"),
+                                dsProtocol("5HT_uncorrelated_octaflow_bypass_100k_97Hz"),
+                                probeName("V8"),
+                                probeDate("")
+      )
     }
 
     "parse uncorrelated dataset names" in {
@@ -68,45 +68,45 @@ class NamingUtilSpec extends WordSpec with Matchers with Inspectors with OptionV
 
       option1 should not be (null)
       option1 shouldBe defined
-      option1.value should have(
-        dsDate("2016_06_15"),
-        dsClass("serotonin"),
-        dsProtocol("5HT_uncorrelated_100k_97Hz"),
-        probeName("A"),
-        probeDate(""))
+      option1.value should have(dsDate("2016_06_15"),
+                                dsClass("serotonin"),
+                                dsProtocol("5HT_uncorrelated_100k_97Hz"),
+                                probeName("A"),
+                                probeDate("")
+      )
 
       val option2 = NamingUtil.datasetInfoFromName("2017_03_09_5HT_uncorrelated_octaflow_bypass_100k_97Hz_V8")
 
       option2 should not be (null)
       option2 shouldBe defined
-      option2.value should have(
-        dsDate("2017_03_09"),
-        dsClass("serotonin"),
-        dsProtocol("5HT_uncorrelated_octaflow_bypass_100k_97Hz"),
-        probeName("V8"),
-        probeDate(""))
+      option2.value should have(dsDate("2017_03_09"),
+                                dsClass("serotonin"),
+                                dsProtocol("5HT_uncorrelated_octaflow_bypass_100k_97Hz"),
+                                probeName("V8"),
+                                probeDate("")
+      )
 
       val option3 = NamingUtil.datasetInfoFromName("2017_03_27_DA_uncorrelated_octaflow_100k_97Hz_S8")
 
       option3 should not be (null)
       option3 shouldBe defined
-      option3.value should have(
-        dsDate("2017_03_27"),
-        dsClass("dopamine"),
-        dsProtocol("DA_uncorrelated_octaflow_100k_97Hz"),
-        probeName("S8"),
-        probeDate(""))
+      option3.value should have(dsDate("2017_03_27"),
+                                dsClass("dopamine"),
+                                dsProtocol("DA_uncorrelated_octaflow_100k_97Hz"),
+                                probeName("S8"),
+                                probeDate("")
+      )
 
       val option4 = NamingUtil.datasetInfoFromName("2016_06_15_DA_RBV1_100k_97Hz_J_EL_2016_06_10")
 
       option4 should not be (null)
       option4 shouldBe defined
-      option4.value should have(
-        dsDate("2016_06_15"),
-        dsClass("dopamine"),
-        dsProtocol("DA_RBV1_100k_97Hz"),
-        probeName("J_EL"),
-        probeDate("2016_06_10"))
+      option4.value should have(dsDate("2016_06_15"),
+                                dsClass("dopamine"),
+                                dsProtocol("DA_RBV1_100k_97Hz"),
+                                probeName("J_EL"),
+                                probeDate("2016_06_10")
+      )
 
     }
 
@@ -115,34 +115,34 @@ class NamingUtilSpec extends WordSpec with Matchers with Inspectors with OptionV
 
       option1 should not be (null)
       option1 shouldBe defined
-      option1.value should have(
-        dsDate("2017_06_22"),
-        dsClass("mixture"),
-        dsProtocol("DA_5HT_NE_octaflow_400Vs_10Hz"),
-        probeName("X1"),
-        probeDate("2017_01_01"))
+      option1.value should have(dsDate("2017_06_22"),
+                                dsClass("mixture"),
+                                dsProtocol("DA_5HT_NE_octaflow_400Vs_10Hz"),
+                                probeName("X1"),
+                                probeDate("2017_01_01")
+      )
 
       val option2 = NamingUtil.datasetInfoFromName("2017_06_22_DA_5HT_NE_octaflow_400Vs_10Hz_X1")
 
       option2 should not be (null)
       option2 shouldBe defined
-      option2.value should have(
-        dsDate("2017_06_22"),
-        dsClass("mixture"),
-        dsProtocol("DA_5HT_NE_octaflow_400Vs_10Hz"),
-        probeName("X1"),
-        probeDate(""))
+      option2.value should have(dsDate("2017_06_22"),
+                                dsClass("mixture"),
+                                dsProtocol("DA_5HT_NE_octaflow_400Vs_10Hz"),
+                                probeName("X1"),
+                                probeDate("")
+      )
 
       val option3 = NamingUtil.datasetInfoFromName("2017_06_22_DA_5HT_NE_octaflow_400Vs_10Hz")
 
       option3 should not be (null)
       option3 shouldBe defined
-      option3.value should have(
-        dsDate("2017_06_22"),
-        dsClass("mixture"),
-        dsProtocol("DA_5HT_NE_octaflow_400Vs_10Hz"),
-        probeName(""),
-        probeDate(""))
+      option3.value should have(dsDate("2017_06_22"),
+                                dsClass("mixture"),
+                                dsProtocol("DA_5HT_NE_octaflow_400Vs_10Hz"),
+                                probeName(""),
+                                probeDate("")
+      )
 
     }
 
@@ -151,34 +151,59 @@ class NamingUtilSpec extends WordSpec with Matchers with Inspectors with OptionV
 
       option1 should not be (null)
       option1 shouldBe defined
-      option1.value should have(
-        dsDate("2018_03_27"),
-        dsClass("mixture"),
-        dsProtocol("5HT_5HIAA_pH_uncorrelated_100k_97Hz"),
-        probeName("CF003"),
-        probeDate("2018_03_24"))
+      option1.value should have(dsDate("2018_03_27"),
+                                dsClass("mixture"),
+                                dsProtocol("5HT_5HIAA_pH_uncorrelated_100k_97Hz"),
+                                probeName("CF003"),
+                                probeDate("2018_03_24")
+      )
 
       val option2 = NamingUtil.datasetInfoFromName("2018_03_27_5HT_5HIAA_pH_uncorrelated_100k_97Hz_CF003")
 
       option2 should not be (null)
       option2 shouldBe defined
-      option2.value should have(
-        dsDate("2018_03_27"),
-        dsClass("mixture"),
-        dsProtocol("5HT_5HIAA_pH_uncorrelated_100k_97Hz"),
-        probeName("CF003"),
-        probeDate(""))
+      option2.value should have(dsDate("2018_03_27"),
+                                dsClass("mixture"),
+                                dsProtocol("5HT_5HIAA_pH_uncorrelated_100k_97Hz"),
+                                probeName("CF003"),
+                                probeDate("")
+      )
 
       val option3 = NamingUtil.datasetInfoFromName("2018_03_27_5HT_5HIAA_pH_uncorrelated_100k_97Hz")
 
       option3 should not be (null)
       option3 shouldBe defined
-      option3.value should have(
-        dsDate("2018_03_27"),
-        dsClass("mixture"),
-        dsProtocol("5HT_5HIAA_pH_uncorrelated_100k_97Hz"),
-        probeName(""),
-        probeDate(""))
+      option3.value should have(dsDate("2018_03_27"),
+                                dsClass("mixture"),
+                                dsProtocol("5HT_5HIAA_pH_uncorrelated_100k_97Hz"),
+                                probeName(""),
+                                probeDate("")
+      )
+
+    }
+
+    "parse same-day repeated probe dataset names" in {
+      val option1 = NamingUtil.datasetInfoFromName("2019_01_07_DA_5HT_NE_5HIAA_uncorrelated_100k_97Hz_MM001W10R13_2")
+
+      option1 should not be (null)
+      option1 shouldBe defined
+      option1.value should have(dsDate("2019_01_07"),
+                                dsClass("mixture"),
+                                dsProtocol("DA_5HT_NE_5HIAA_uncorrelated_100k_97Hz"),
+                                probeName("MM001W10R13"),
+                                probeDate("")
+      )
+
+      val option2 = NamingUtil.datasetInfoFromName("2018_03_27_5HT_5HIAA_pH_uncorrelated_100k_97Hz_CF003_2018_03_24_2")
+
+      option2 should not be (null)
+      option2 shouldBe defined
+      option2.value should have(dsDate("2018_03_27"),
+                                dsClass("mixture"),
+                                dsProtocol("5HT_5HIAA_pH_uncorrelated_100k_97Hz"),
+                                probeName("CF003"),
+                                probeDate("2018_03_24")
+      )
 
     }
 
@@ -190,50 +215,50 @@ trait CustomMatchers {
   def dsClass(expectedValue: String) =
     new HavePropertyMatcher[InvitroDataset, String] {
       def apply(info: InvitroDataset) =
-        HavePropertyMatchResult(
-          info.dsClass == expectedValue,
+        HavePropertyMatchResult(info.dsClass == expectedValue,
           "dsClass",
           expectedValue,
-          info.dsClass)
+          info.dsClass
+        )
     }
 
   def dsDate(expectedValue: String) =
     new HavePropertyMatcher[InvitroDataset, String] {
       def apply(info: InvitroDataset) =
-        HavePropertyMatchResult(
-          info.dsDate == expectedValue,
+        HavePropertyMatchResult(info.dsDate == expectedValue,
           "dsDate",
           expectedValue,
-          info.dsDate)
+          info.dsDate
+        )
     }
 
   def dsProtocol(expectedValue: String) =
     new HavePropertyMatcher[InvitroDataset, String] {
       def apply(info: InvitroDataset) =
-        HavePropertyMatchResult(
-          info.dsProtocol == expectedValue,
+        HavePropertyMatchResult(info.dsProtocol == expectedValue,
           "dsProtocol",
           expectedValue,
-          info.dsProtocol)
+          info.dsProtocol
+        )
     }
 
   def probeName(expectedValue: String) =
     new HavePropertyMatcher[InvitroDataset, String] {
       def apply(info: InvitroDataset) =
-        HavePropertyMatchResult(
-          info.probeName == expectedValue,
+        HavePropertyMatchResult(info.probeName == expectedValue,
           "probeName",
           expectedValue,
-          info.probeName)
+          info.probeName
+        )
     }
 
   def probeDate(expectedValue: String) =
     new HavePropertyMatcher[InvitroDataset, String] {
       def apply(info: InvitroDataset) =
-        HavePropertyMatchResult(
-          info.probeDate == expectedValue,
+        HavePropertyMatchResult(info.probeDate == expectedValue,
           "probeDate",
           expectedValue,
-          info.probeDate)
+          info.probeDate
+        )
     }
 }
