cd54db3cb51abfdf76f9eebc94c4ad24a845deed
Jason White <jas0nw@vtc.vt.edu>
2020-04-29 15:58:29 -0400

    added script to pull config files from cluster

---
+++ src/main/matlab/pull-config.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+#--chown=jwhite:admin \
+#sudo 
+rsync \
+    -aiH \
+    --include=Chem.m \
+    --include=Config.m \
+    --include=TargetCatalog.m \
+    --include=TestingCatalog.m \
+    --include=TrainingCatalog.m \
+    --safe-links \
+    --exclude="*.m" \
+    --exclude="*.m~" \
+    --exclude="*.swp" \
+    --exclude=log/ \
+    --exclude=.DS_Store \
+    --exclude=push.sh \
+    --exclude=pull.sh \
+    --exclude=.nfs.* \
+    --exclude=target/ \
+    --exclude=process_test.m \
+    --no-p \
+    --no-o \
+    --no-g \
+    $@ hnl02:/mnt/nfs/proj/in-vitro/iterate/src/ ./
