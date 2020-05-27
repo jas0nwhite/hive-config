e3c1b222049722205a0d089cda32d56b2c00e2f6
Jason White <jas0nw@vtc.vt.edu>
2020-05-27 12:57:40 -0400

    work-in-progress

--- src/main/matlab/process.m
+++ src/main/matlab/process.m
@@ -1,51 +1,114 @@
+%% config
+!./make-config treatment-24.conf
+% !./make-config treatment-013-008.conf
+
+
 %% init
+restoredefaultpath;
+clear RESTOREDEFAULTPATH_EXECUTED;
+clear classes; %#ok<CLCLS>
+
 clc;
-cfg = Config;
+cfg = Config.init();
+
+overwrite = false;
+parallel = true;
+R2017a = strcmp(version('-release'), '2017a');
+
+% set default mat-file version to v6 for speed -- files will be big!
+rootgroup = settings();
+rootgroup.matlab.general.matfile.SaveFormat.PersonalValue = 'v6';
+
+if parallel && isempty(gcp('nocreate'))
+    parpool(feature('numcores')); % number of cross-validation folds
+end
+
+% jitterCorrector = hive.proc.invitro.JitterCorrector()...
+%     .withExamWindow(1:30);
+defaultJitterCorrector = [];
 
 
 %% import
 t = tic;
-hive.proc.train.Importer(cfg).withOverwrite(false).process();
+hive.proc.train.Importer(cfg)...
+    .inParallel(parallel)...
+    .withOverwrite(overwrite)...
+    .withJitterCorrector(defaultJitterCorrector)...
+    .withUnderflowCorrection()...
+    .purgeExcludedData()...
+    .process();
 toc(t);
 
 t = tic;
-hive.proc.test.Importer(cfg).withOverwrite(false).process();
+hive.proc.test.Importer(cfg)...
+    .inParallel(parallel)...
+    .withOverwrite(overwrite)...
+    .withJitterCorrector(defaultJitterCorrector)...
+    .withUnderflowCorrection()...
+    .purgeExcludedData()...
+    .process();
 toc(t);
 
 
 %% summarize
 t = tic;
-hive.proc.train.Summarizer(cfg).withOverwrite(false).process().plot();
+hive.proc.train.Summarizer(cfg)...
+    .inParallel(parallel)...
+    .withOverwrite(overwrite)...
+    .process()...
+    .plot();
 toc(t);
 
 t = tic;
-hive.proc.test.Summarizer(cfg).withOverwrite(false).process().plot();
+hive.proc.test.Summarizer(cfg)...
+    .inParallel(parallel)...
+    .withOverwrite(overwrite)...
+    .process()...
+    .plot();
 toc(t);
 
 
-%% characterize
-t = tic;
-hive.proc.train.Characterizer(cfg).withOverwrite(false).process();
-toc(t);
+%% CLUSTER-STYLE: characterize voltammograms using knee location
+% t = tic;
+% hive.proc.train.KneeCharacterizer(cfg)...
+%     .inParallel(parallel)...
+%     .withOverwrite(overwrite)...
+%     .process();
+% toc(t);
+% 
+% t = tic;
+% hive.proc.test.KneeCharacterizer(cfg)...
+%     .inParallel(parallel)...
+%     .withOverwrite(overwrite)...
+%     .process();
+% toc(t);
 
-t = tic;
-hive.proc.test.Characterizer(cfg).withOverwrite(false).process();
-toc(t);
 
-
-%% index
-t = tic;
-hive.proc.train.Indexer(cfg).withOverwrite(false).process().plot();
-toc(t);
-
-t = tic;
-hive.proc.test.Indexer(cfg).withOverwrite(false).process().plot();
-toc(t);
+%% CLUSTER-STYLE: index with DBSCAN clustering to identify and reject outliers
+% t = tic;
+% hive.proc.train.Indexer(cfg)...
+%     .inParallel(parallel)...
+%     .withOverwrite(overwrite)...
+%     .process()...
+%     .plot();
+% toc(t);
+% 
+% t = tic;
+% hive.proc.test.Indexer(cfg)...
+%     .inParallel(parallel)...
+%     .withOverwrite(overwrite)...
+%     .process()...
+%     .plot();
+% toc(t);
 
 
 %% build index cloud for clustering
 t = tic;
-hive.proc.train.Aggregator(cfg).process();
+hive.proc.train.Aggregator(cfg)...
+    .inParallel(parallel)...
+    .withOverwrite(overwrite)...
+    .process()...
+    .plot();
 toc(t);
 
 
